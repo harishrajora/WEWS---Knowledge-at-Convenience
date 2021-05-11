a54a977eddb20e5e8f64f098425532f279793d98
Harish Rajora <harrylitterell@yahoo.in>
2021-05-11 23:18:11 +0530

    UI changed. New API added.

--- WEWS/popup.js
+++ WEWS/popup.js
@@ -1,6 +1,6 @@
 var country; //Global variable for country
 var descriptionOfNews;
-
+var apiKey = "1a8191b0cc194deaa28117bd7e9722c0";
 function pseudo(){
 var whatsnewvariable = document.querySelector("#whatsNew");
 whatsnewvariable.addEventListener("click", whatsNew);
@@ -12,7 +12,7 @@ function whatsNew(){
   });
   //initializing whats new popup
   $(document).ready(function(){
-  $('#whatsNew').popover({title: "<h4>Version 3.3</h4>", content: "<h5><b><em>Now you can search for specific news through keyword<br><br>Now you can redirect to the full coverage page by clicking the button<br><br>Now the color of Headline changes once you click it</em></b></h5>", html: true, placement: "auto bottom", animation: true});
+  $('#whatsNew').popover({title: "<h4>Version 4.1</h4>", content: "<h5><b><em>UI Improvements<br><br>Bug Fixes<br><br>Death Counter Added For Corona</em></b></h5>", html: true, placement: "auto bottom", animation: true});
   });
 };
 document.addEventListener('DOMContentLoaded', function getLocation() {
@@ -52,6 +52,7 @@ function showPosition(position) {
                 var weatherdesc= res.weather[0];
                 weatherdesc= weatherdesc["main"];
                 country = res.sys.country;
+                covid(country);
                 var selectCountry = document.querySelector("#country");
                 selectCountry.innerHTML = country;
                 newsapi();
@@ -68,6 +69,7 @@ function showPosition(position) {
 };
 
 
+
 //Show News on the homepage
 function newsapi(){
   pseudo();
@@ -79,16 +81,17 @@ var keyword = document.getElementById("keywordInput").value;
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = JSON.parse(this.responseText);
+        console.log(myObj.results)
         var news = document.querySelector("#news"); //embed into the html
-        var articlesObj= myObj.articles[0];  //selects the first object
+        var articlesObj= myObj.results[0];  //selects the first object
         var titleOfNews = articlesObj.title;
-        var source = articlesObj.source.name;
-        var image = articlesObj.urlToImage;
+        // var source = articlesObj.url;
+        var image = articlesObj.multimedia[0].url;
         var url = articlesObj.url;
         document.querySelector("#image").src = image;
         document.querySelector("#source").innerHTML = source;
         document.querySelector("#linkToNews").href = url;
-        descriptionOfNews = articlesObj.description;
+        descriptionOfNews = articlesObj.abstract;
         news.innerHTML = titleOfNews;
         news.addEventListener('click' , function(){
           if(this.innerHTML == titleOfNews)
@@ -107,13 +110,13 @@ xmlhttp.onreadystatechange = function() {
           news.style.fontSize = "24px";
           news.dataToggle = "tooltip";
           news.addEventListener('mouseup', function(){
-            news.style.color = "black";
+            news.style.color = "white";
           });
         }
         });
       }
     };
-xmlhttp.open("GET", "https://newsapi.org/v2/top-headlines?country="+country+"&apiKey=1a8191b0cc194deaa28117bd7e9722c0" , true);
+xmlhttp.open("GET", "https://api.nytimes.com/svc/topstories/v2/world.json?api-key=tdOL5gSKx1XGm7ElitCaV17YeOejZY8s" , true);
 xmlhttp.send();
 };
 
@@ -164,12 +167,63 @@ function NewsThroughKeyword(){
           news.style.fontSize = "24px";
           news.dataToggle = "tooltip";
           news.addEventListener('mouseup', function(){
-            news.style.color = "black";
+            news.style.color = "white";
           });
         }
       });
     }
   };
-    xmlhttp.open("GET", "https://newsapi.org/v2/everything?q="+keyword+"&apiKey=1a8191b0cc194deaa28117bd7e9722c0" , true);
+    xmlhttp.open("GET", "https://newsapi.org/v2/everything?q="+keyword+"&apiKey="+apiKey , true);
     xmlhttp.send();
     };
+
+
+function covid(country){
+  var xmlhttp= "";
+  if(window.XMLHttpRequest)
+      {
+          xmlhttp= new XMLHttpRequest();
+      }
+  else{
+      xmlhttp= new ActiveXObject("Microsoft.XMLHTTP");
+  }
+
+  xmlhttp.onreadystatechange= function()
+  {
+      //alert(this.readyState+" "+this.status);
+      if(this.readyState == 4 && this.status ==200)
+          {
+              //alert(xmlhttp.responseText);
+              document.getElementById("coronatracker").style.visibility = "visible";
+              var res= JSON.parse(xmlhttp.responseText);
+              var countries = res.Countries;
+              var i = 0;
+              for(i = 0; i < countries.length; i++){
+                if(countries[i].CountryCode == country)
+                break;
+              }
+              var india = countries[i].Country;
+              var newConfirmed = countries[i].NewConfirmed;
+              var totalConfirmed = countries[i].TotalConfirmed;
+              var newDeaths = countries[i].NewDeaths;
+              var totalDeaths = countries[i].TotalDeaths;
+              var newRecovered = countries[i].NewRecovered;
+              var totalRecovered = countries[i].TotalRecovered;
+              //document.getElementById("country").innerHTML = india;
+              //document.getElementById("totalrecovered").innerHTML = "Total Recovered Cases: "+ totalRecovered;
+              //document.getElementById("newrecovered").innerHTML = "New Recovered Cases: "+ newRecovered;
+              document.getElementById("totaldeath").innerHTML = totalDeaths;
+              document.getElementById("newdeath").innerHTML =  newDeaths;
+              // document.getElementById("totalcases").innerHTML = "Total Confirmed Cases : " + totalConfirmed;
+              document.getElementById("totalcases").innerHTML = totalConfirmed;
+              document.getElementById("newcases").innerHTML =  newConfirmed;
+              document.getElementById("countryname").innerHTML = india;;
+
+            }
+        else {
+          //document.getElementById("temperature").innerHTML = "Unable to Load Weather, Check your connection";
+        }
+          };
+  xmlhttp.open("GET", "https://api.covid19api.com/summary", true);
+  xmlhttp.send();
+  };
