974fb6259e4cf2784272115de1c501050a754225
Harish Rajora <harrylitterell@yahoo.in>
2021-06-05 02:26:19 +0530

    New UI. NY Times added.

--- WEWS/popup.js
+++ WEWS/popup.js
@@ -1,9 +1,12 @@
 var country; //Global variable for country
 var descriptionOfNews;
 var apiKey = "1a8191b0cc194deaa28117bd7e9722c0";
+var counter = 0;
 function pseudo(){
 var whatsnewvariable = document.querySelector("#whatsNew");
 whatsnewvariable.addEventListener("click", whatsNew);
+var nn = document.querySelector("#next_news");
+nn.addEventListener("click", next_news);
 };
 function whatsNew(){
   //initializing click me!! tooltip
@@ -12,7 +15,7 @@ function whatsNew(){
   });
   //initializing whats new popup
   $(document).ready(function(){
-  $('#whatsNew').popover({title: "<h4>Version 4.1</h4>", content: "<h5><b><em>UI Improvements<br><br>Bug Fixes<br><br>NY Times Added To Source List</em></b></h5>", html: true, placement: "auto bottom", animation: true});
+  $('#whatsNew').popover({title: "<h4>Version 4.2.4</h4>", content: "<h5><b><em>UI Improvements<br><br>Next Button Working</em></b></h5>", html: true, placement: "auto bottom", animation: true});
   });
 };
 document.addEventListener('DOMContentLoaded', function getLocation() {
@@ -51,13 +54,15 @@ function showPosition(position) {
                 var place= res.name;
                 var weatherdesc= res.weather[0];
                 weatherdesc= weatherdesc["main"];
+                wind_speed = res.wind["speed"];
+                // wind_speed = wind_speed["speed"];
                 country = res.sys.country;
                 covid(country);
                 var selectCountry = document.querySelector("#country");
                 selectCountry.innerHTML = country;
                 newsapi();
                 //alert(weatherdesc);
-                document.getElementById("temperature").innerHTML= tempc+"<sup>&#8451</sup>" + " , "+ weatherdesc+" in  "+place;
+                document.getElementById("temperature").innerHTML= tempc+"<sup>&#8451</sup>" + " , "+ weatherdesc+" in  "+place + " / Wind: "+ wind_speed +"m/s";
               }
           else {
             document.getElementById("temperature").innerHTML = "Unable to Load Weather, Check your connection";
@@ -69,23 +74,44 @@ function showPosition(position) {
 };
 
 
+var myObj;
+news_list = [];
 
 //Show News on the homepage
 function newsapi(){
   pseudo();
   //var harish = document.querySelector("#globalNews");
-  document.querySelector("#globalNewsButton").addEventListener("click", NewsThroughKeyword);
+  // document.querySelector("#globalNewsButton").addEventListener("click", NewsThroughKeyword);
   //console.log(harish);
 var xmlhttp = new XMLHttpRequest();
-var keyword = document.getElementById("keywordInput").value;
+// var keyword = document.getElementById("keywordInput").value;
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        var myObj = JSON.parse(this.responseText);
-        console.log(myObj.results)
-        var news = document.querySelector("#news"); //embed into the html
+        myObj = JSON.parse(this.responseText);
+        // console.log(myObj.results)
+        // var news = document.querySelector("#news"); //embed into the html
+
+        var i;
+        var articlesObj;
+        console.log(myObj.results.length);
+        for(i = 0; i < myObj.results.length; i++){
+          new_obj = {};
+          articlesObj = myObj.results[i];
+          new_obj["title"] = articlesObj.title;
+          new_obj["description"] = articlesObj.abstract;
+          if(articlesObj.multimedia !=  null)
+          new_obj["image"] = articlesObj.multimedia[0].url;
+          else
+          new_obj["image"] = "null";
+          new_obj["url"] = articlesObj.url;
+          news_list.push(new_obj);
+        }
+        console.log(news_list);
         var articlesObj= myObj.results[0];  //selects the first object
         var titleOfNews = articlesObj.title;
+          var news = document.querySelector("#news");
         // var source = articlesObj.url;
+        console.log(titleOfNews);
         var image = articlesObj.multimedia[0].url;
         var url = articlesObj.url;
         document.querySelector("#image").src = image;
@@ -93,33 +119,78 @@ xmlhttp.onreadystatechange = function() {
         document.querySelector("#linkToNews").href = url;
         descriptionOfNews = articlesObj.abstract;
         news.innerHTML = titleOfNews;
-        news.addEventListener('click' , function(){
-          if(this.innerHTML == titleOfNews)
-          {
-          news.innerHTML = descriptionOfNews;
-          news.style.fontStyle = "italic";
-          news.style.fontSize = "20px";
-          news.dataToggle= "xyz";
-          news.addEventListener('mouseup', function(){
-            news.style.color = "rgb(134,122,210)";
-          })
-        }
-        else {
-          news.innerHTML = titleOfNews;
-          news.style.fontStyle = "normal";
-          news.style.fontSize = "24px";
-          news.dataToggle = "tooltip";
-          news.addEventListener('mouseup', function(){
-            news.style.color = "white";
-          });
-        }
-        });
+        // news.addEventListener('click' , function(){
+        //   console.log(this.innerHTML);
+        //   console.log("Title of News" + titleOfNews);
+        //   if(this.innerHTML == titleOfNews)
+        //   {
+        //   console.log("Inside If");
+        //   news.innerHTML = descriptionOfNews;
+        //   news.style.fontStyle = "normal";
+        //   news.style.fontSize = "20px";
+        //   news.dataToggle= "tooltip";
+        //   news.addEventListener('mouseup', function(){
+        //     news.style.color = "rgb(134,122,210)";
+        //   })
+        // }
+        // else {
+        //   console.log("Inside Else");
+        //   news.innerHTML = titleOfNews;
+        //   news.style.fontStyle = "normal";
+        //   news.style.fontSize = "24px";
+        //   news.dataToggle = "tooltip";
+        //   news.addEventListener('mouseup', function(){
+        //     news.style.color = "white";
+        //   });
+        // }
+        // });
+        counter++;
+
       }
     };
 xmlhttp.open("GET", "https://api.nytimes.com/svc/topstories/v2/world.json?api-key=tdOL5gSKx1XGm7ElitCaV17YeOejZY8s" , true);
 xmlhttp.send();
 };
 
+function next_news(){
+    counter++;
+    if(counter >= myObj.results.length)
+    counter = 0;
+
+  var news = document.querySelector("#news");
+  var titleOfNews = news_list[counter].title;
+ document.querySelector("#image").src = news_list[counter].image;
+ document.querySelector("#linkToNews").href = news_list[counter].url;
+ descriptionOfNews = news_list[counter].abstract;
+ news.innerHTML = titleOfNews;
+
+//   news.addEventListener('click' , function(){
+//     console.log(this.innerHTML);
+//     console.log("Title of News" + titleOfNews);
+//     if(this.innerHTML == titleOfNews)
+//     {
+//     console.log("Inside If");
+//     news.innerHTML = descriptionOfNews;
+//     news.style.fontStyle = "normal";
+//     news.style.fontSize = "20px";
+//     news.dataToggle= "tooltip";
+//     news.addEventListener('mouseup', function(){
+//       news.style.color = "rgb(134,122,210)";
+//     })
+// }
+//   else {
+//     console.log("Inside Else");
+//     news.innerHTML = titleOfNews;
+//     news.style.fontStyle = "normal";
+//     news.style.fontSize = "24px";
+//     news.dataToggle = "tooltip";
+//     news.addEventListener('mouseup', function(){
+//       news.style.color = "white";
+//     });
+//   }
+// });
+
+}
 
 //
 //Show news about a specific keyword
